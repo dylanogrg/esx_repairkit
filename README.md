45e52f85a12f73134e5feafc6fb5bd6351c08fc0
dylanogrg <129014956+dylanogrg@users.noreply.github.com>
2023-10-17 20:58:06 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+### esx_repairkit
+
+Eenvoudig reparatieKit -script voor Fivem ESX -servers
+
+script staat helemaal in het nederlands
+
+### benodigte scripts
+* es_extended
+
+### instal
+Download het script en vernoem de folder naar esx_repairkit.
+
+Sleep de map in uw map `<server-data>/resources/[esx]` en voeg dit toe aan uw server.cfg
+```
+start esx_repairkit
+```
---
+++ locales/nl.lua
@@ -0,0 +1,8 @@
+Locales ['nl'] = {
+	['used_kit']			= 'U gebruikte ~y~ x1 ~b~ reparatieKit',
+	['must_be_outside']		= 'U moet buiten het voertuig zijn!',
+	['no_vehicle_nearby']		= 'Er is geen ~r~ voertuig ~w~ in de buurt',
+	['finished_repair']		= '~G~ u hebt het voertuig gerepareerd!',
+	['abort_hint']			= 'Druk op ~input_veh_duck~ om te annuleren',
+	['aborted_repair']		= 'u ~r~ afgebroken ~w~ de reparaties',
+}
